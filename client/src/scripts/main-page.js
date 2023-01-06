4372e5c0b6fc82524deeb79b52571f79cc0ca04b
Maksim <m.ivanov@itpelag.com>
2023-01-06 11:47:15 +0300

    added slider and dropdown menu for catalog

--- client/src/scripts/main-page.js
+++ client/src/scripts/main-page.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     new Swiper("#main-slider", {
+        loop: true,
         navigation: {
             nextEl: "#main-slider-button-next",
             prevEl: "#main-slider-button-prev",
