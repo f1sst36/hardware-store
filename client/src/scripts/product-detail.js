5ad27839b68538d1ba4f2da500091c74bd9c50c8
F1sst <f1sst36@gmail.com>
2023-04-10 22:01:35 +0300

    started make the cart

--- client/src/scripts/product-detail.js
+++ client/src/scripts/product-detail.js
@@ -15,4 +15,24 @@ document.addEventListener('DOMContentLoaded', () => {
             swiper: swiper,
         },
     });
+
+    const buyButton = document.getElementById('buy-button')
+    if(!buyButton) return
+
+    buyButton.addEventListener('click', () => {
+        const counterInput = document.querySelector('*[data-textfield-type="counter"] input')
+        const itemsCount = counterInput.value || 1
+        Cookies.set('cartProductIds', JSON.stringify([1, 2, 3]))
+        console.log(JSON.parse(Cookies.get('cartProductIds')))
+        // let cartProductIds = null
+        // try {
+        //     cartProductIds = JSON.parse(localStorage.getItem('cartProductIds'))
+        // }catch(e) {}
+        //
+        // if(!cartProductIds) {
+        //     cartProductIds = []
+        // }
+        //
+        // cartProductIds.push(productId)
+    })
 })
